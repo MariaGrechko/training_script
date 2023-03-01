862870d8c1da2a3747fab2691f89626755114d5d
Grechko Maria <grechko.maria2003@gmail.com>
2023-03-01 10:59:28 +0300

    Fix program bugs

--- script.rb
+++ script.rb
@@ -1,20 +1,18 @@
 # frozen_string_literal: true
 
-# rubocop:todo Style/Documentation
-class Histogram_Generator # rubocop:todo Naming/ClassAndModuleCamelCase, Style/Documentation
-  # rubocop:enable Style/Documentation
+class HistogramGenerator
   def initialize; end
 
-  def self.process_String(string:) # rubocop:todo Naming/MethodName
-    result = if !string.empty? # rubocop:todo Lint/UselessAssignment
+  def self.process_string(string:)
+    if !string.empty?
 
-               string.gsub(/[ ,.\r\n]/, ' ').split(' ').each_with_object(Hash.new(0)) do |word, result|
-                 result[word] += 1
-               end
+      string.gsub(/[ ,.\r\n]/, ' ').split(' ').each_with_object(Hash.new(0)) do |word, result|
+        result[word] += 1
+      end
 
-             else
-               {}
-             end
+    else
+      {}
+    end
   end
 
   def self.sort_histogram(histogram)
@@ -23,30 +21,25 @@ class Histogram_Generator # rubocop:todo Naming/ClassAndModuleCamelCase, Style/D
   end
 end
 
-module App # rubocop:todo Style/Documentation
-  # rubocop:todo Lint/MissingCopEnableDirective
-  # rubocop:todo Naming/ConstantName
-  # rubocop:enable Lint/MissingCopEnableDirective
-  Text = 'When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story
-  starts, there was nothing about the cloudy sky outside to suggest that
+module App
+  TEXT = 'When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story
+starts, there was nothing about the cloudy sky outside to suggest that
 strange and mysterious things would soon be happening all over the
 country. Mr. Dursley hummed as he picked out his most boring tie for
 work, and Mrs. Dursley gossiped away happily as she wrestled a screaming
 Dudley into his high chair.'
   def perform
-    text = ''
     puts('reading file')
-    puts("extracted string: #{text}")
-    histogram = Histogram_Generator.processString({ string: Text })
+    puts("extracted string: #{TEXT}")
+    histogram = HistogramGenerator.process_string(string: TEXT)
     puts('generated histogram:')
     puts(histogram)
     puts('sorted histogram:')
-    puts(Histogram_Generator.sort_histogram(histogram))
+    puts(HistogramGenerator.sort_histogram(histogram))
   end
   module_function :perform
 
-  # rubocop:todo Metrics/MethodLength
-  def number_to_word(number) # rubocop:todo Metrics/CyclomaticComplexity, Metrics/MethodLength
+  def to_word(number)
     case number
     when 1
       'one'
@@ -70,11 +63,10 @@ Dudley into his high chair.'
       'oops'
     end
   end
-  # rubocop:enable Metrics/MethodLength
-  module_function :number_to_word
+  module_function :to_word
 end
 
 puts App.perform
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 13_666].each do |number|
-  puts App.number_to_word(number)
+  puts App.to_word(number)
 end
